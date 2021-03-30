b7d0a210f63ab33b301e6b46bdd4b587aa115246
Daniel Moreno <DeNialDev@users.noreply.github.com>
2021-03-30 01:19:32 -0600

    SplashScreen

--- app/src/main/java/com/example/ezmarket/MainActivity.java
+++ app/src/main/java/com/example/ezmarket/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ezmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        };
+        thread.start();
     }
 }
